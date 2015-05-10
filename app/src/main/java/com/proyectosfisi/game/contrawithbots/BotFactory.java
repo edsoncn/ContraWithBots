b24f6536480167a92fed407d8af3406c9214873e
edsoncn <edsedx4@gmail.com>
2015-05-10 15:50:10 -0500

    .

--- app/src/main/java/com/proyectosfisi/game/contrawithbots/BotFactory.java
+++ app/src/main/java/com/proyectosfisi/game/contrawithbots/BotFactory.java
@@ -4,4 +4,11 @@ package com.proyectosfisi.game.contrawithbots;
  * Created by edson on 10/05/2015.
  */
 public class BotFactory {
+
+private ArrayList<PersonajeEnemigo> bots;
+
+private BotFactory(){
+}
+
+
 }
